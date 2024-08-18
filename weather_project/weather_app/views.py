61574a24354ef34812a9eece498612e31a043c1c
Djohnny14 <superboy9500@gmail.com>
2024-08-18 10:46:22 +0500

    second

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,39 +1,29 @@
 from django.shortcuts import render
 import requests
-import datetime
 
 def index(request):
 
-    current_weather_url = "https://geocoding-api.open-meteo.com/v1/search?name={}&count=1&language=en&format=json"
+    coordinate_url = "https://geocoding-api.open-meteo.com/v1/search?name={}&count=1&language={}&format=json"
     forecast_url = "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=auto&forecast_days=7"
 
     if request.method == 'POST':
-        city1 = request.POST['city1']
-        city2 = request.POST.get('city2', None)
-
-        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, current_weather_url, forecast_url)
-
-        if city2:
-            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, current_weather_url,
-                                                                         forecast_url)
-        else:
-            weather_data2, daily_forecasts2 = None, None
-
+        city = request.POST['city']
+        lang=request.POST.get('lang')
+        weather_data, daily_forecasts = fetch_weather_and_forecast(city, lang, coordinate_url, forecast_url)
         context = {
-            'weather_data1': weather_data1,
-            'daily_forecasts1': daily_forecasts1,
-            'weather_data2': weather_data2,
-            'daily_forecasts2': daily_forecasts2,
+            'weather_data': weather_data,
+            'daily_forecasts': daily_forecasts,
         }
-
         return render(request, 'weather_app/index.html', context)
     else:
         return render(request, 'weather_app/index.html')
 
 
-def fetch_weather_and_forecast(city, current_weather_url, forecast_url):
-    response = requests.get(current_weather_url.format(city)).json()
+def fetch_weather_and_forecast(city, lang, coordinate_url, forecast_url):
+    response = requests.get(coordinate_url.format(city,lang)).json()
+    print(coordinate_url.format(city,lang))
     lat, lon = response['results'][0]['latitude'], response['results'][0]['longitude']
+    print(f'Latitude:{lat}; Longitude:{lon}')
     forecast_response = requests.get(forecast_url.format(lat, lon)).json()
 
 
@@ -50,6 +40,5 @@ def fetch_weather_and_forecast(city, current_weather_url, forecast_url):
             'min_temp': forecast_response['daily']['temperature_2m_min'][i],
             'max_temp': forecast_response['daily']['temperature_2m_max'][i]
         })
-
 
     return weather_data, daily_forecasts
